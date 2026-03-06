11065fa20534c408a3910010837c5ddb3850f7c0
TheProtoBoi <apeterson0916@gmail.com>
2026-03-06 10:18:57 -0600

    Create level.js

---
+++ level.js
@@ -0,0 +1,15 @@
+// Predefined obstacles for short "Stereo Madness" level
+obstacles = [
+  { x: 800, y: 310, w: 40, h: 40 },
+  { x: 1200, y: 310, w: 40, h: 40 },
+  { x: 1600, y: 310, w: 40, h: 40 },
+  { x: 2000, y: 310, w: 40, h: 40 },
+  { x: 2400, y: 310, w: 40, h: 40 },
+  { x: 2800, y: 310, w: 40, h: 40 },
+  { x: 3200, y: 310, w: 40, h: 40 },
+
+  // spikes
+  { x: 1000, y: 350-20, w: 20, h: 20, type: "spike" },
+  { x: 1800, y: 350-20, w: 20, h: 20, type: "spike" },
+  { x: 2600, y: 350-20, w: 20, h: 20, type: "spike" }
+];
