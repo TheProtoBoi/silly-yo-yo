99d404fc470a8fc3ee163f9c3e0ad56c92270fa5
TheProtoBoi <apeterson0916@gmail.com>
2026-03-06 10:11:54 -0600

    Create game.js

---
+++ game.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+// Draw ground
+function drawGround() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, player.ground, canvas.width, 5);
+}
+
+// Main game loop
+function loop() {
+  // Update
+  updatePlayer();
+  updateObstacles();
+
+  // Draw
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawGround();
+  drawPlayer(ctx);
+  drawObstacles(ctx);
+
+  requestAnimationFrame(loop);
+}
+
+// Start the game
+loop();
