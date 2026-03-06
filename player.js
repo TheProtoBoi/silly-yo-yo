10e2a0c0c907187c5ca334e28fd68c1ea8859cd5
TheProtoBoi <apeterson0916@gmail.com>
2026-03-06 10:17:50 -0600

    Update player.js

--- player.js
+++ player.js
@@ -7,7 +7,9 @@ const player = {
   jumping: false,
   gravity: 1,
   ground: 350,
-  jumpPower: -18
+  jumpPower: -18,
+  rotation: 0,
+  rotationSpeed: 0
 };
 
 // Jump control
@@ -15,6 +17,7 @@ document.addEventListener("keydown", e => {
   if (e.code === "Space" && !player.jumping) {
     player.velocityY = player.jumpPower;
     player.jumping = true;
+    player.rotationSpeed = 0.3; // rotate while jumping
   }
 });
 
@@ -27,11 +30,20 @@ function updatePlayer() {
     player.y = player.ground - player.size;
     player.velocityY = 0;
     player.jumping = false;
+    player.rotation = 0;
+    player.rotationSpeed = 0;
   }
+
+  // rotate cube
+  player.rotation += player.rotationSpeed;
 }
 
 // Draw player
 function drawPlayer(ctx) {
+  ctx.save();
+  ctx.translate(player.x + player.size/2, player.y + player.size/2);
+  ctx.rotate(player.rotation);
   ctx.fillStyle = "cyan";
-  ctx.fillRect(player.x, player.y, player.size, player.size);
+  ctx.fillRect(-player.size/2, -player.size/2, player.size, player.size);
+  ctx.restore();
 }
